9a93d1069b1e3cac407aaeefaab68ef5ceb34133
sachinsebastian1212 <43450034+sachinsebastian1212@users.noreply.github.com>
2024-10-16 19:43:45 +0530

    showed placedetails screen on tapping list item

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_favorite_places/models/place.dart';
+import 'package:flutter_favorite_places/screens/place_details.dart';
 
 class PlacesList extends StatelessWidget {
   const PlacesList({super.key, required this.places});
@@ -29,6 +30,11 @@ class PlacesList extends StatelessWidget {
               .titleMedium!
               .copyWith(color: Theme.of(context).colorScheme.onSurface),
         ),
+        onTap: () {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => PlaceDetailsScreen(place: places[index]),
+          ));
+        },
       ),
       itemCount: places.length,
     );
