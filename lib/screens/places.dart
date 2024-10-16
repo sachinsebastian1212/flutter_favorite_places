04e2937227b537d7e875881efdb5b296b6b11801
sachinsebastian1212 <sachinsebastian12@gmail.com>
2024-10-16 16:45:11 +0530

    added text field and button in add place screen

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_favorite_places/screens/add_place.dart';
 import 'package:flutter_favorite_places/widgets/places_list.dart';
 
 class PlacesScreen extends StatelessWidget {
@@ -9,9 +10,9 @@ class PlacesScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your places'),
-        actions: [
-          IconButton(onPressed: (){}, icon: const Icon(Icons.add))
-        ],
+        actions: [IconButton(onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) => const AddPlaceScreen(),));
+        }, icon: const Icon(Icons.add))],
       ),
       body: const PlacesList(places: []),
     );
